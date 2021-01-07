498de58cf4c95525a2cef6e9d949aea6ffe101bb
Clodéric Mars <cloderic@hey.com>
2021-01-07 12:35:31 +0100

    Fixing the material on the loaded primitive

--- webapp/src/Navmesh.js
+++ webapp/src/Navmesh.js
@@ -1,5 +1,6 @@
 import React, { useMemo } from 'react';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { MeshBasicMaterial } from 'three';
 
 const Navmesh = ({ navmeshObj, color }) => {
   const navmesh = useMemo(() => {
@@ -7,16 +8,16 @@ const Navmesh = ({ navmeshObj, color }) => {
     const scene = loader.parse(navmeshObj);
     return scene.children[0];
   }, [navmeshObj]);
+  const material = useMemo(() => new MeshBasicMaterial({ color }), [color]);
 
   return (
     <primitive
       object={navmesh}
       position={[0, 0, 0]}
       rotation={[-Math.PI / 2, 0, 0]}
+      material={material}
       receiveShadow
-    >
-      <meshStandardMaterial color={color} />
-    </primitive>
+    />
   );
 };
 export default Navmesh;
